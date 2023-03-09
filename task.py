880e8d991fb0b9deb180ab9227c78f2ad7ff6109
Yevhenii Mynenko <68693183+Yevheniy2020@users.noreply.github.com>
2023-03-09 23:10:21 +0200

    done task

--- task.py
+++ task.py
@@ -1,2 +1,12 @@
 #Дано масив словників.
 #Реалізувати пошук по ключах кожного словника і вивести кількість знайдених результатів на екран.
+
+arr = [{'First': 'Apple', 'Second': True}, {2: "Third", 'First': 100}, {'First': False, 2: 'SSS'}]
+print('Enter str')
+str = input()
+counter = 0
+for element in   arr:
+   if element.get(str):
+       counter += 1
+
+print(counter)
